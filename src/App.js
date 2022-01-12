0ec3ff942fc8adc2934bf4c07546f0a30ff892b7
llama-with-thumbs <vlad.filitovich@gmail.com>
2022-01-12 15:52:52 -0500

    init: charts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Routes>
+        <Route exact path="/" component={HomePage} />
+      </Routes> */}
+      <Header />
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './homepage.styles.scss';
+const HomePage = () => (
+    <main className="homepage">
+        
+    </main>
+);
+
+export default HomePage;
