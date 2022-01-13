b939155e57e3e38199e59094071be7c77e8edde4
llama-with-thumbs <vlad.filitovich@gmail.com>
2022-01-13 15:46:03 -0500

    feat: create avg-temp-chart component

---
+++ src/components/avg-temp-chart/AxisLeft.js
@@ -0,0 +1,15 @@
+export const AxisLeft = ({ yScale, innerWidth, tickOffset = 3 }) =>
+  yScale.ticks().map((tickValue) => (
+    <g className="tick" transform={`translate(0,${yScale(tickValue)})`}>
+      <line x2={innerWidth} />
+      <text
+        key={tickValue}
+        style={{ textAnchor: 'end' }}
+        x={-tickOffset}
+        dy=".32em"
+        y={yScale(tickValue)}
+      >
+        {tickValue}
+      </text>
+    </g>
+  ));
